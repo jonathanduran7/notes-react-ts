de31193d1cafa237aa83477e1b171b3a7f48da7a
Jonathan <78237375+jonathanduran7@users.noreply.github.com>
2022-06-07 20:43:24 -0300

    added list notes

--- src/pages/notes/components/BarraLateral.tsx
+++ src/pages/notes/components/BarraLateral.tsx
@@ -1,15 +1,33 @@
-import { TextField } from "@mui/material"
+import { Button, Divider, List, ListItem, ListItemText, TextField } from "@mui/material"
+import { NotesData } from '../../../utils/NotesData'
+
+const style = {
+   width: '100%',
+   maxWidth: 360,
+   bgcolor: 'background.paper',
+};
 
 const BarraLateral = () => {
    return (
       <div className="barraLateral">
          {/* Buscador */}
          <div>
-            <TextField id="standard-basic" label="Search" variant="standard" />
+            <TextField id="standard-basic" label="Search" variant="standard" fullWidth />
          </div>
 
          {/* lista de ntoas */}
-         <div>lista de notas</div>
+         <div className="listNotes">
+            <List sx={style} component="nav" aria-label="mailbox folders">
+               {
+                  NotesData.map(note => (
+                     <ListItem button divider>
+                        <ListItemText primary={`${note.title}`} />
+                     </ListItem>
+                  ))
+               }
+
+            </List>
+         </div>
       </div>
    )
 }
---
+++ src/utils/NotesData.ts
@@ -0,0 +1,27 @@
+export const NotesData = [
+   {
+      id: 1,
+      title: 'Example',
+      content: 'loremasdfsadfsadfsadfsdfsd'
+   },
+   {
+      id: 2,
+      title: 'Example 2',
+      content: 'loremasdfsadfsadfsadfsdfsd'
+   },
+   {
+      id: 3,
+      title: 'Example 3',
+      content: 'loremasdfsadfsadfsadfsdfsd'
+   },
+   {
+      id: 4,
+      title: 'Example 4',
+      content: 'loremasdfsadfsadfsadfsdfsd'
+   },
+   {
+      id: 5,
+      title: 'Example 5',
+      content: 'loremasdfsadfsadfsadfsdfsd'
+   },
+]
