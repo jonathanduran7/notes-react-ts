2e9e6f7d7210d01e59f3c883a2dec83cab97ab14
Jonathan <78237375+jonathanduran7@users.noreply.github.com>
2022-06-07 20:31:00 -0300

    added input

--- src/pages/notes/components/BarraLateral.tsx
+++ src/pages/notes/components/BarraLateral.tsx
@@ -1,8 +1,17 @@
+import { TextField } from "@mui/material"
 
 const BarraLateral = () => {
-  return (
-    <div className="barraLateral">BarraLateral</div>
-  )
+   return (
+      <div className="barraLateral">
+         {/* Buscador */}
+         <div>
+            <TextField id="standard-basic" label="Search" variant="standard" />
+         </div>
+
+         {/* lista de ntoas */}
+         <div>lista de notas</div>
+      </div>
+   )
 }
 
 export default BarraLateral
