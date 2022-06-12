421f8edcb2dc984a11897180a2d192ea38da20e6
Jonathan <78237375+jonathanduran7@users.noreply.github.com>
2022-06-12 12:42:57 -0300

    created task component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css';
 import { NotesProvider } from './pages/notes/context/NotesProvider';
 import NotesPage from './pages/notes/Index';
+import TasksPage from './pages/tasks/TasksPage';
 
 function App() {
   return (
     <div className="App">
-      <NotesProvider>
-        <NotesPage />
-      </NotesProvider>
+      {/* <NotesProvider> */}
+        {/* <NotesPage /> */}
+      {/* </NotesProvider> */}
+
+      <TasksPage />
     </div>
   );
 }
---
+++ src/pages/tasks/TasksPage.tsx
@@ -0,0 +1,15 @@
+const TasksPage = () => {
+   return (
+      <div>
+         <div>
+            barra lateral
+         </div>
+
+         <div>
+            lista de tareas
+         </div>
+      </div>
+   )
+}
+
+export default TasksPage
