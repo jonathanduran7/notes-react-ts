4f325aeb7d2a4481d700cafcdec9b72da34e2579
jduran <joniduran624@gmail.com>
2022-06-18 20:42:04 -0300

    modified test category modal

--- src/test/tasks/CreateCategoryModal.test.tsx
+++ src/test/tasks/CreateCategoryModal.test.tsx
@@ -1,39 +1,33 @@
 import '@testing-library/jest-dom/extend-expect'
-import { fireEvent, prettyDOM, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../../App';
-import TasksPage from '../../pages/tasks/TasksPage';
 
 describe('Category Modal', () => {
 
+   let component = render(<App />)
 
-   test('render component', ()=> {
-      let component = render(<TasksPage />)
+   beforeEach(()=> {
+      let component = render(<App />)
       let btnCategory: any = component.container.querySelector('#crear-categoria-btn')
       userEvent.click(btnCategory)
+   })
+
+   test('render component', ()=> {
       component.getByText('Crear Categoria Modal')
    })
 
    test('close modal', () => {
-      let component = render(<TasksPage />)
-      let btnCategory: any = component.container.querySelector('#crear-categoria-btn')
-      userEvent.click(btnCategory)
-
       let btnCancelar = component.getByText('Cancelar')
       userEvent.click(btnCancelar)
    })
 
    test('create caregory', () => {
-      let component = render(<App />)
-      let btnCategory: any = component.container.querySelector('#crear-categoria-btn')
-      userEvent.click(btnCategory)
-
       let btnConfirmar = component.getByText('Confirmar')
       let input = screen.getByPlaceholderText('Inserte Titulo')
-
-      userEvent.type(input, 'Universidad')
+      let valueForInpt = 'Trabajo'
+      userEvent.type(input, valueForInpt)
       userEvent.click(btnConfirmar)
-
-      expect(component.container).toHaveTextContent('Universidad')
+      screen.getByText(valueForInpt)
    })
 })
